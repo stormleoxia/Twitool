d4a16efafaf7337de56cc2f82080276a83d97501
storm <storm@leoxia.com>
2015-09-21 18:39:48 +0700

    Add ProtoPoc

--- Lx.Web.Twitter/Lx.Web.Twitter.Console/CredentialManager.cs
+++ Lx.Web.Twitter/Lx.Web.Twitter.Console/CredentialManager.cs
@@ -7,23 +7,38 @@ namespace Lx.Web.Twitter.Console
 {
     public class CredentialManager : ICredentialManager
     {
+        private readonly IFileSystem _fileSystem;
+        private readonly IConsole _console;
         private const string CredentialBase = "Lx.Twitter/credentials";
 
-        public CredentialManager()
+        public CredentialManager(IFileSystem fileSystem, IConsole console)
         {
-
+            _fileSystem = fileSystem;
+            _console = console;
         }
 
+        /// <summary>
+        /// Retrieves the credentials on local machine,
+        /// If not available, ask for them.
+        /// </summary>
+        /// <returns></returns>
         public ITwitterCredentials RetrieveCredentials()
         {
             ITwitterCredentials credentials;
-            var credentialsRetrieved = TryGetCredentials(out credentials);
-            while (!credentialsRetrieved)
+            var credentialsRetrieved = TryGetStoredCredentialsOnLocalMachine(out credentials);
+            while (!credentialsRetrieved) // No Credentials on local machine, ask for them
             {
-                AskForCredentials(out credentials);
+                credentials = AskForCredentials();
                 credentialsRetrieved = ValidateCredentials(credentials);
             }
+            StoreCredentialsOnLocalMachine(credentials);
+            Auth.SetUserCredentials(credentials.ConsumerKey, credentials.ConsumerSecret, credentials.AuthorizationKey, credentials.AuthorizationSecret);
             return credentials;
+        }
+
+        private void StoreCredentialsOnLocalMachine(ITwitterCredentials credentials)
+        {
+            
         }
 
         private bool ValidateCredentials(ITwitterCredentials credentials)
@@ -31,33 +46,53 @@ namespace Lx.Web.Twitter.Console
             return true;
         }
 
-        private void AskForCredentials(out ITwitterCredentials credentials)
+        private ITwitterCredentials AskForCredentials()
         {
-            credentials = new TwitterCredentials();
+            var applicationCredentials = new ConsumerCredentials("consumer_key", "consumer_secret");
+            var url = CredentialsCreator.GetAuthorizationURL(applicationCredentials);
+            _console.WriteLine("Navigate to : {0}", url);
+            _console.WriteLine("Enter the captcha: ");
+            var captcha = _console.ReadLine();
+            var newCredentials = CredentialsCreator.GetCredentialsFromVerifierCode(captcha, applicationCredentials);
+            if (newCredentials == null)
+            {
+                _console.Error.WriteLine("Bad captcha");
+            }
+            else
+            {
+                _console.WriteLine("Access Token = {0}", newCredentials.AccessToken);
+                _console.WriteLine("Access Token Secret = {0}", newCredentials.AccessTokenSecret);
+            }
+            return newCredentials;
         }
 
-        private bool TryGetCredentials(out ITwitterCredentials credentials)
+        private bool TryGetStoredCredentialsOnLocalMachine(out ITwitterCredentials credentials)
         {
-            credentials = new TwitterCredentials
-            {
-                ConsumerKey = "",
-                ConsumerSecret = "",
-                AuthorizationKey = "",
-                AuthorizationSecret = "",
-            };
+            credentials = null;
             var appPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
             var credentialFileName = Path.Combine(appPath, CredentialBase);
-/*            if (!_fileSystem.FileExists(credentialFileName))
+            if (!_fileSystem.FileExists(credentialFileName))
             {
                 return false;
             }
-            using (var reader = _fileSystem.OpenText(credentialFileName))
+            using (var reader = _fileSystem.Open(credentialFileName))
             {
-                var result = reader.ReadToEnd();
-
-            }*/
-            Auth.SetUserCredentials(credentials.ConsumerKey, credentials.ConsumerSecret, credentials.AuthorizationKey, credentials.AuthorizationSecret);
-            return true;
+                
+            }
+            return false;
         }
+    }
+
+    public interface IFileSystem
+    {
+        bool FileExists(string filePath);
+        IDisposable Open(string filePath);
+    }
+
+    public interface IConsole
+    {
+        void WriteLine(string format, params object[] parameters);
+        string ReadLine();
+        IConsole Error { get; }
     }
 }
---
+++ Lx.Web.Twitter/Lx.Web.Twitter.Console/SerializedCredentials.cs
@@ -0,0 +1,17 @@
+﻿using Tweetinvi.Core.Credentials;
+
+namespace Lx.Web.Twitter.Console
+{
+    
+    public class SerializedCredentials
+    {
+        public string ConsumerKey { get; set; }
+
+        public string ConsumerSecret { get; set; }
+
+        public string AuthorizationKey { get; set; }
+
+        public string AuthorizationSecret { get; set; }
+
+    }
+}
